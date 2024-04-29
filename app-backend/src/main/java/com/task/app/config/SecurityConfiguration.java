c057249a32bd09d4484a0169f8a10e0d723dd30a
rgarcia <robertoremyg@gmail.com>
2024-04-29 05:35:13 -0400

    feat: SecurityConfiguration implemented, pom.xml modified

---
+++ app-backend/src/main/java/com/task/app/config/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.task.app.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->{
+                    auth.requestMatchers("/helloWorld").permitAll();
+                })
+                .build();
+    }
+}
